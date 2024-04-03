5b2289f7cf8b18ac1e558ea87bc01c1cb7e263da
Axelle <axelle.mazuy@gmail.com>
2024-04-03 11:13:17 +0200

    feature: add GetController

---
+++ src/main/java/fr/univrouen/cv24/controllers/GetController.java
@@ -0,0 +1,29 @@
+package fr.univrouen.cv24.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetController {
+    @GetMapping("/resume")
+    public String getListCVinXML() {
+        return "Envoi de la liste des CV";
+    }
+
+    @GetMapping("/cvid")
+    public String getCVinXML(
+        @RequestParam(value = "texte") String texte)
+    {
+        return "Détail du contenu du CV n°" + texte;
+    }
+
+    @GetMapping("/test")
+    public String getTest(
+            @RequestParam(value = "id") int id,
+            @RequestParam(value = "titre") String titre)
+    {
+        return "Test :<br>" + "id = " + id + "<br>titre = " + titre;
+    }
+
+}
